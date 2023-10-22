bc587cc56611044b98163a372d6915502e97dfc0
nsabir0 <nsakib472@gmail.com>
2023-10-22 19:03:50 +0600

    login/registration all completed...

--- lib/View/home_page.dart
+++ lib/View/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:material_login_ui/Controller/profile_controller.dart';
 import 'package:material_login_ui/Global_Widgets/my_editfield.dart';
-import 'package:material_login_ui/Model/userData_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,7 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final ProfileController _controller = Get.put(ProfileController());
-  userData_Model userData = Get.arguments;
+  var userData = Get.arguments;
 /*  List country_list = [ "Bangla", "Hindi", "Urdu"];
 
   void _incrementList() {
@@ -47,13 +46,13 @@ class _HomePageState extends State<HomePage> {
 
               spacing(10),
 
-              MyEditField(controller: _controller.nameController, hintText: userData.name),
-              MyEditField(controller: _controller.emailController, hintText: userData.email),
-              MyEditField(controller: _controller.passController, hintText: userData.password),
+              MyEditField(controller: _controller.nameController, hintText: userData[0]),
+              MyEditField(controller: _controller.emailController, hintText: userData[1]),
+              MyEditField(controller: _controller.passController, hintText: userData[2]),
               spacing(10),
 
               ElevatedButton(onPressed: ()async{
-                //_controller.DeleteUser(userData.id);
+                _controller.DeleteUser(userData[1]);
 
               },
                   style:
